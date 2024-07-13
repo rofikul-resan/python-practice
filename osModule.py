a7b79f2a8ac525d9967b0bdd7b04b4d31371dd65
rofikul-resan <resan6231@gmail.com>
2024-07-13 23:13:15 +0600

    try string methode in py

--- osModule.py
+++ osModule.py
@@ -4,5 +4,7 @@ import os
 
 directory_path="/"
 file_and_dirs= os.listdir(directory_path)
+
+# list the all dirs using loop
 for item in file_and_dirs:
     print(item)
---
+++ stringPracrtce.py
@@ -0,0 +1,7 @@
+# practice string methode
+
+name = "resan"
+
+sub_name= name[-4 : -1]
+print(len(name))
+print(sub_name)
