0a135bdb3823b9959d3fa5466d0a153f003320b3
rofikul-resan <resan6231@gmail.com>
2024-07-13 23:30:26 +0600

    try string function in py

--- stringPracrtce.py
+++ stringPracrtce.py
@@ -5,3 +5,18 @@ name = "resan"
 sub_name= name[-4 : -1]
 print(len(name))
 print(sub_name)
+
+# try skiping value by   index
+full_name = "rofikul islam resan"
+
+print(full_name[2:9:2])
+print(full_name[2::2])
+
+# print(full_name[2:9:1]) #[start index : end index : numder of skiping value ] 
+print(full_name[2::3])
+
+
+# ------------------------
+# string function practice 
+# -------------------------
+print(len(full_name))
