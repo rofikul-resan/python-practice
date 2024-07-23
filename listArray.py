47a2a4925922a1c7cc34bf9b8e2187abfd606986
rofikul-resan <resan6231@gmail.com>
2024-07-23 14:39:40 +0600

    python array method practice

---
+++ listArray.py
@@ -0,0 +1,11 @@
+
+
+# list of number
+number=[12,415,5,545,4,25,48,634,45,85,647,58,56,75,97,5,746,75,67,456,9,76,8,7,6,874,69,7] 
+
+# list of parson name
+parson=["resan" , "resad", "asik" ,"maruf" , "suci" , "rayat" , "niloy" , "nayme"]
+
+number.sort()
+number.reverse()
+print(number)
