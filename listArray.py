01bcc1b7616692f050808f225a19e5785cd14e0a
rofikul-resan <resan6231@gmail.com>
2024-07-23 15:01:05 +0600

    practice tuple adn array methode

--- listArray.py
+++ listArray.py
@@ -8,4 +8,18 @@ parson=["resan" , "resad", "asik" ,"maruf" , "suci" , "rayat" , "niloy" , "nayme
 
 number.sort()
 number.reverse()
-print(number)
+# print(number)
+
+parson.append("siam")
+parson.insert(2 , "rofikul")
+# parson.remove("rayat")
+# parson.pop(len(parson)-2)
+# parson.pop(len(parson)- 1)
+# print(parson)
+
+
+# tuple list in python
+num_tuple = (1,12,25,1,0,0,11,0,2,0,3,0)
+
+# print(num_tuple.count(14))
+print(num_tuple.index(25))
