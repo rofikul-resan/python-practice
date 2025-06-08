cc71bb0b071fe44ebfd823ba77a6d8c4ce3d21c8
rofikul-resan <resan6231@gmail.com>
2025-06-08 13:12:51 +0600

    todo input print style

---
+++ Todoapp.py
@@ -0,0 +1,17 @@
+while True :
+    print("1. All Todo")
+    print("2. Add Todo")
+    print("3. Delete Todo")
+    print ("4. Exit")
+    choose = int(input("Choose  one : "))
+
+    match choose :
+        case 1 :
+            print("All Todo")
+        case 2 :
+            print("Add Todo")
+        case 3 :
+            print("Delete Todo")
+        case 4 : 
+            break
+    
